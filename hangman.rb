3407268b6f90a7b5731044842609aca3cae6d8e0
Jerliyah <jerliyahcraig@gmail.com>
2017-02-21 22:12:33 -0600

    taking turns functionality and successful abort for end game

--- hangman.rb
+++ hangman.rb
@@ -19,6 +19,8 @@
 # Read 5desk line by line
 dictionary = File.readlines "5desk.txt"
 
+
+
 # Filter words that are 5 to 12 characters long
 filtered_words = []
 
@@ -28,9 +30,18 @@ dictionary.each do |word|
     end
 end
 
+
+
 # Pick a word from the filtered list at random
 GAME_WORD = filtered_words[ rand(filtered_words.length - 1) ]
 puts GAME_WORD
+
+
+
+# Global player variables so that all functions know if the user or computer is taking their turn
+$player = :user
+$mistakes_left = 3
+
 
 
 # Right or wrong alert
@@ -43,21 +54,72 @@ def wrong_answer
     puts "\nNope, not that letter"
 
     # Countdown of mistakes left
-    mistakes_left -= 1
-end
-
-# Interface for guessing letters (one player against comp)
-# [Start of with random letters -> random letters in a certain range of known letter -> guess next later based on known words in dictionary]
-puts "Please type in a letter"
-guess_letter = gets.chomp
-
-if GAME_WORD.include? guess_letter
-    right_answer
-else
-    wrong_answer
+    $mistakes_left -= 1
 end
 
 
+def right_or_wrong(guess_letter)
+    if GAME_WORD.include? guess_letter
+        right_answer
+    else
+        wrong_answer
+    end
+end
+
+
+
+def user_takes_turn
+    $player = :user
+
+    # Interface for guessing letters (one player against comp)
+    puts "Please type in a letter"
+    guess_letter = gets.chomp
+
+    right_or_wrong(guess_letter)
+
+    taking_turns(:comp)
+end
+
+
+def comp_takes_turn
+    $player = :comp
+
+    # [Start of with random letters -> random letters in a certain range of known letter -> guess next later based on known words in dictionary]
+    randIndex = rand(26)
+    guess_letter = ('a'..'z').to_a[randIndex]
+
+    right_or_wrong(guess_letter)
+
+    taking_turns(:user)
+end
+
+
+def close_game
+    puts "That's the end of the game!"
+    abort
+end
+
+
+def between_turns
+    if $mistakes_left <= 0 then
+        close_game
+    end
+end
+
+
+def taking_turns(player)
+
+    between_turns
+
+    if player == :user
+        user_takes_turn
+    else
+        comp_takes_turn
+    end
+end
+
+
+taking_turns($player)
 
 
 # Save game functionality 
